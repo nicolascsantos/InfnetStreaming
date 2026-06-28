fd2991d33e23f6b813556f3123abb0cff3594d00
Nicolas de Carvalho Santos <nicolas.carvalho@hotmail.com.br>
2026-06-28 16:10:52 -0300

    adicionando conexao banco de dados projeto api

---
+++ src/InfnetStreaming.API/Configurations/ConfiguracoesConexao.cs
@@ -0,0 +1,22 @@
+﻿using InfnetStreaming.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace InfnetStreaming.API.Configurations
+{
+    public static class ConfiguracoesConexao
+    {
+        public static IServiceCollection AdicionarConexoesApp(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbConnection(configuration);
+            return services;
+        }
+
+        public static IServiceCollection AddDbConnection(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<InfnetStreamingDbContext>(options =>
+                options.UseInMemoryDatabase("DbInfnetStreaming")
+            );
+            return services;
+        }
+    }
+}
